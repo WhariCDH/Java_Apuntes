05a5ce4b495365948aa2ae67e48ac395671d2905
WhariCDH <huaripatagarcia@gmail.com>
2025-09-26 14:21:40 -0500

    Aumento

---
+++ Clase02/while13.java
@@ -0,0 +1,12 @@
+//El programa debe impromir números del 1 al 5
+package Clase02;
+public class while13 {
+    
+    public static void main(String[] args) {
+        int i=1;
+        while (i<=5){
+            System.out.println("El número es: "+i);
+            i++; //Incrementa i en 1
+        }
+    }
+}
